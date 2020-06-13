6e9c561c9e5373d21f71e1df00aa9ffc20bf98a6
Andrei Mandychev <andrei.mandychev-ext@adevinta.com>
2020-06-14 00:23:14 +0200

    Add Facebook button

--- App/Sample/build.gradle.kts
+++ App/Sample/build.gradle.kts
@@ -24,6 +24,7 @@ android {
         }
     }
 
+    viewBinding.isEnabled = true
 }
 
 dependencies {
--- App/Sample/src/main/java/fr/dappli/externaldaggercomponent/MainActivity.kt
+++ App/Sample/src/main/java/fr/dappli/externaldaggercomponent/MainActivity.kt
@@ -2,11 +2,15 @@ package fr.dappli.externaldaggercomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.dappli.externaldaggercomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
