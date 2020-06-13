85e7c0736df8b6a750cd448ddc311e092d552694
Andrei Mandychev <andrei.mandychev-ext@adevinta.com>
2020-06-14 00:22:43 +0200

    Add Facebook SDK

--- App/Sample/build.gradle.kts
+++ App/Sample/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 android {
     compileSdkVersion(29)
 
-
     defaultConfig {
         applicationId = "fr.dappli.externaldaggercomponent"
         minSdkVersion(21)
@@ -39,7 +38,8 @@ dependencies {
         "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${PluginsVersions.kotlin}",
         "androidx.appcompat:appcompat:1.1.0",
         "androidx.core:core-ktx:1.2.0",
-        "androidx.constraintlayout:constraintlayout:1.1.3"
+        "androidx.constraintlayout:constraintlayout:1.1.3",
+        "com.facebook.android:facebook-login:[5,6)"
     )
     implementationProjects(
         ":Sdk:Network"
